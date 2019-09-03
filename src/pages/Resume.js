cdf463c82e5a17b294bc6e8a21eab29431e4dc68
Mason J. Katz <Mason.Katz@gmail.com>
2019-09-03 14:55:04 -0700

    QL is the default

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -1,31 +1,80 @@
 import React from 'react'
-
+import { graphql } from 'gatsby'
 import Navbar from 'react-bootstrap/Navbar'
-import Card from 'react-bootstrap/Card'
+import {
+  Section,
+  Employer,
+  Department,
+  Job,
+  Bullet,
+  Degree,
+  Publication,
+} from '../components/Resume'
 
-import { Section } from '../components/Resume'
 import { Experience } from '../components/Experience'
-import { Publications } from '../components/Publications'
 import { Education } from '../components/Education'
+import { Publications } from '../components/Publications'
 
 import 'bootstrap/dist/css/bootstrap.min.css'
 
-export default () => (
+const extract = (data, key) => {
+  // Return a single array with only non-null value for the KEY lookup
+  let items = data.allResumeYaml.edges
+    .map(({ node }) => node[key])
+    .filter(item => item !== null)
+
+  return items.reduce((array, elem) => array.concat(elem), [])
+}
+
+export default ({ data }) => (
   <>
     <Navbar bg="light" expand="lg" sticky="top">
       <Navbar.Brand>masonkatz.com</Navbar.Brand>
     </Navbar>
-    <Card title="Experience">
-      <Card.Header>Experience</Card.Header>
-      <Card.Body>
-        <Experience />
-      </Card.Body>
-    </Card>
+    <Section title="Experience">
+      <Experience data={extract(data, 'experience')} />
+    </Section>
     <Section title="Publications">
-      <Publications />
+      <Publications data={extract(data, 'publication')} />
     </Section>
     <Section title="Education">
-      <Education />
+      <Education data={extract(data, 'degree')} />
     </Section>
   </>
 )
+
+export const query = graphql`
+  query {
+    allResumeYaml {
+      edges {
+        node {
+          experience {
+            company
+            web
+            position {
+              department
+              web
+              title
+              dates
+              desc
+            }
+          }
+          degree {
+            school
+            degree
+            major
+            date
+          }
+          publication {
+            title
+            authors
+            journal
+            issue
+            web
+            pdf
+          }
+        }
+      }
+    }
+  }
+`
